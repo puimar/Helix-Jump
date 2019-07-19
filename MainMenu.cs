6bc99c7cc6543b8aa42269ca14e2e66179eca8fe
puimar <53020042+puimar@users.noreply.github.com>
2019-07-19 22:10:10 +0300

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+{
+    public Button newGameButton;
+    public Button NoAdsButton;
+    public Button exitGameButton;
+    
+    public string newGameSceneName;
+    
+    public void Awake() {
+        newGameButton.onClick.AddListener(newGame);
+        exitGameButton.onClick.AddListener(exitGame);
+    }
+    public void newGame() {
+        SceneManager.LoadScene(newGameSceneName);
+    }
+
+    public void NoAds() {
+
+    }
+
+    public void exitGame() {
+        Application.Quit();
+    }
+
+}
